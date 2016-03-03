9797eece7dac3238339b91d013ed5f51573f8bfb
Jenn Leung <jleung@jleung.ascendify.com>
2016-03-02 18:02:19 -0800

    got tray popping up

--- CraveApp/HomeViewController.swift
+++ CraveApp/HomeViewController.swift
@@ -9,11 +9,37 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet var trayView: UIView!
+    @IBOutlet var foodTextBox: UITextField!
+    var trayViewOriginalCenter: CGPoint!
+    var trayViewController: UIViewController!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //setting of original trayView coordinates
+        trayViewOriginalCenter = CGPoint(x: trayView.center.x, y: trayView.center.y)
+        
+        //hiding until activated
+        trayView.alpha = 0
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+//        trayViewController = storyboard.instantiateViewControllerWithIdentifier("TrayViewController") as!TrayViewController
+//        trayView.addSubview(trayViewController.view)
+//        trayViewController.didMoveToParentViewController(self)
+        
+        
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func onTapFood(sender: AnyObject) {
+        print("on tap")
+        self.trayView.alpha = 1
+        
+        UIView.animateWithDuration(0.4, delay: 0, options: [], animations: { () -> Void in
+            self.trayView.center.y = self.trayViewOriginalCenter.y
+            print("on animate")
+            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
